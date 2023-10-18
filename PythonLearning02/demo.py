da656e73d93c4eed29c1ce2bd9291caa487cb5cc
zhangwen666666 <1813505899@qq.com>
2023-10-18 19:09:18 +0800

    23-10-18

--- PythonLearning02/demo.py
+++ PythonLearning02/demo.py
@@ -1,6 +1,7 @@
 # import Module1
 import time
 
+
 # import time
 #
 # time.sleep(10)
@@ -88,49 +89,179 @@ import time
 # print(s1.__repr__())
 
 
-import numpy as np
-import matplotlib.pyplot as plt
+# import numpy as np
+# import matplotlib.pyplot as plt
+#
+# A = np.array([[0, 1, -1], [-1, 0, 1]])
+#
+# # 奇异值分解
+# U, s, V = np.linalg.svd(A)
+#
+# # 零空间和左零空间的基向量
+# null_space = V[-1]
+# left_null_space = U[:, -1]
+#
+# # 创建一个三维图形
+# fig = plt.figure()
+# ax = fig.add_subplot(111, projection='3d')
+#
+# # 绘制零空间中的向量
+# ax.quiver(0, 0, 0, null_space[0], null_space[1], null_space[2], color='blue')
+#
+# # 绘制左零空间中的向量
+# ax.quiver(0, 0, 0, left_null_space[0], left_null_space[1], 0, color='red')
+#
+# # 列空间和行空间的基向量
+# column_space = np.linalg.qr(A)[0]
+# row_space = np.linalg.qr(A.T)[0]
+#
+# # 绘制列空间中的向量
+# for vector in column_space.T:
+#     ax.quiver(0, 0, 0, vector[0], vector[1], 0, color='green')
+#
+# # 绘制行空间中的向量
+# for vector in row_space.T:
+#     ax.quiver(0, 0, 0, vector[0], vector[1], 0, color='black')
+#
+# # 设置坐标轴范围
+# ax.set_xlim([-0.5, 1])
+# ax.set_ylim([-0.5, 1])
+# ax.set_zlim([-0.5, 1])
+#
+# # 设置坐标轴标签
+# ax.set_xlabel('X')
+# ax.set_ylabel('Y')
+# ax.set_zlabel('Z')
+#
+# # 显示图形
+# plt.show()
 
-A = np.array([[0, 1, -1], [-1, 0, 1]])
 
-# 奇异值分解
-U, s, V = np.linalg.svd(A)
+# class Student(object):
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#
+#     def __gt__(self, other):
+#         return self.age > other.age
+#
+#     def __ge__(self, other):
+#         return self.age >= other.age
+#
+#     def __int__(self):
+#         return 20
+#
+#
+# s1 = Student("张三", 18)
+# print(int(s1))  # 20
+#
+# s2 = Student("李四", 18)
+# print(s1 >= s2)  # True
+# print(s1 < s2)
 
-# 零空间和左零空间的基向量
-null_space = V[-1]
-left_null_space = U[:, -1]
 
-# 创建一个三维图形
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
+# class Person(object):
+#     """
+#     这是一个人类
+#     """
+#
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#
+#     def eat(self):
+#         print(self.name + '正在吃饭')
+#
+#
+# p = Person('张三', 18)
+# print(dir(p))
+# print(p.__dir__())
+# print(p.__doc__)  # 这是一个人类
+# print(p.__dict__)  # {'name': '张三', 'age': 18}
+# print(p.__class__)  # <class '__main__.Person'>
+# print(p.__module__)  # __main__
 
-# 绘制零空间中的向量
-ax.quiver(0, 0, 0, null_space[0], null_space[1], null_space[2], color='blue')
 
-# 绘制左零空间中的向量
-ax.quiver(0, 0, 0, left_null_space[0], left_null_space[1], 0, color='red')
+# class Student(object):
+#     def __init__(self, name, age, city):
+#         self.name = name
+#         self.age = age
+#         self.city = city
+#
+#     def __setitem__(self, key, value):
+#         # self.key = value  不会将原属性修改，而是会多一个self.key属性并且值为value
+#         self.__dict__[key] = value
+#
+#     def __getitem__(self, item):
+#         return self.__dict__[item]
+#
+# s1 = Student("张三", 25, "上海")  # {'name': '张三', 'age': 25, 'city': '上海'}
+# print(s1['age'])    #25
+#
+# print(s1.__dict__)  # 把对象转换成为字典
+# s1['name'] = "李四"
+# print(s1.__dict__)  # {'name': '李四', 'age': 25, 'city': '上海'}
 
-# 列空间和行空间的基向量
-column_space = np.linalg.qr(A)[0]
-row_space = np.linalg.qr(A.T)[0]
 
-# 绘制列空间中的向量
-for vector in column_space.T:
-    ax.quiver(0, 0, 0, vector[0], vector[1], 0, color='green')
+# class Student(object):
+#
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#         self.__money = 1000
+#
+#     def __demo(self):
+#         print("私有函数__demo")
+#
+#     def test(self):
+#         self.__money += 1000
+#         self.__demo()
+#
+# s1 = Student("张三", 18)
+# s1._Student__demo() #私有函数__demo
+# s1.test()   #私有函数__demo
+#
+# print(s1.name, s1.age)
+# print(s1._Student__money)   #1000
 
-# 绘制行空间中的向量
-for vector in row_space.T:
-    ax.quiver(0, 0, 0, vector[0], vector[1], 0, color='black')
+# print(Student.type)  # 'person'
+# print(s1.type)  # 'person'
+# print(s2.type)  # 'person'
+# s1.type = 'human'  # 不会修改类属性type，而是s1对象多了一个对象属性type，值为'human'
+# print(Student.type)  # 'person'
+# print(s1.type)  # 'human'
+# print(s2.type)  # 'person'
+# print(s1.__dict__)  # {'name': '张三', 'age': 18, 'type': 'human'}
+# print(s2.__dict__)  # {'name': '李四', 'age': 29}
+# # 对象s1和s2都是由Student类创建出来的对象
+# # name和age是对象属性，是每一个实例对象都会单独保存一份的属性
+# # 每个实例对象之间的属性没有关联，互不影响
+#
+# x = s1
+# print(x.name)  # 张三
+# # print(Student.name)
 
-# 设置坐标轴范围
-ax.set_xlim([-0.5, 1])
-ax.set_ylim([-0.5, 1])
-ax.set_zlim([-0.5, 1])
 
-# 设置坐标轴标签
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-ax.set_zlabel('Z')
+class Singleton:
+    __instance = None  # 私有的类属性
+    __is_first = True
 
-# 显示图形
-plt.show()
+    @classmethod
+    def __new__(cls, *args, **kwargs):
+        if cls.__instance == None:
+            cls.__instance = object.__new__(cls)
+        return cls.__instance
+
+    def __init__(self, a, b):
+        if self.__is_first:
+            self.a = a
+            self.b = b
+            self.__is_first = False
+
+s1 = Singleton("heheh", "hahah")
+s2 = Singleton("哈哈哈", "嘿嘿嘿")
+print(s1 is s2)  # True
+print(s1.a)  # heheh
+print(s1.b)  # hahah
+print(s2.a)  # heheh
+print(s2.b)  # hahah
